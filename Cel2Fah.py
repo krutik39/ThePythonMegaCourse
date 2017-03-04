1fa95990a8747b6e149f7dc52faedae7f39668f4
krutik39 <krutik391@gmail.com>
2017-03-03 17:34:15 -0800

    Changes

--- Cel2Fah.py
+++ Cel2Fah.py
@@ -1,8 +1,16 @@
-def cel2fah(cel):
-    if cel < -273.15:
-        print ("Invalid Temperature")
-    else:
-        fah = (((cel*9)/5)+32)
-        return fah
-
-print (cel2fah(-300))
+# def cel2fah(cel):
+#         if cel < -273.15:
+#             print ("Invalid Temperature")
+#         else:
+#             fah = (((cel*9)/5)+32)
+#             return fah
+# print (cel2fah(-300))
+# temperatures = [10,-20,-289,100]
+def temps(l1):
+    for i in l1:
+        if i < -273.15:
+            print("Invalid Temperature")
+        else:
+            j = (((i*9)/5)+32)
+            print (j)
+print (temps([10,-20,-289,100]))
---
+++ mergeList.py
@@ -0,0 +1,5 @@
+name = ['krutik','harsh','kunal']
+email = ['gmail','aol','orkut']
+
+for i,j in zip(name,email):
+    print(i,j)
