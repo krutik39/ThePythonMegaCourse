c4611384e2589cb88c26fa204b264849c64bc168
krutik39 <krutik391@gmail.com>
2017-03-03 16:07:58 -0800

    function

--- myprogram.py
+++ myprogram.py
@@ -1,2 +1,9 @@
-print("Hello")
-print(3 + 4)
+age = input("Enter your age: ")
+new_age = float(age) + 50
+print(new_age)
+
+def currency_converter(rate, euros):
+    dollars = euros * rate
+    return dollars
+
+print (currency_converter(100,1000))
