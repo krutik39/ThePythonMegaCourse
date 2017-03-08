cf0bad286950601ac59e52d38c25b9506fbf9b13
krutik39 <krutik391@gmail.com>
2017-03-07 16:00:12 -0800

    Datetime

---
+++ example.py
@@ -0,0 +1,14 @@
+import datetime
+
+r"""
+This script creates an empty file
+"""
+
+filename=datetime.datetime.now()
+
+def create_file():
+    """This function creates an empty file"""
+    with open(filename.strftime("%Y-%m-%d-%H-%M")+".txt","w") as file:
+        file.write("") #Writing an empty string.
+
+create_file()
