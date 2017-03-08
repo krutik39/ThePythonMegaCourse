91c3a0042d3457e38c072e1743e26e8715da52bb
krutik39 <krutik391@gmail.com>
2017-03-07 16:39:41 -0800

    Section 6 Solution

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ filename=datetime.datetime.now()
 
 def create_file():
     """This function creates an empty file"""
-    with open(filename.strftime("%Y-%m-%d-%H-%M")+".txt","w") as file:
+    with open(filename.strftime("%Y-%m-%d-%H-%M-%S")+".txt","w") as file:
         file.write("") #Writing an empty string.
 
 create_file()
